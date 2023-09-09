1afca6a6ee62b96fd68255f20348e469a8f3fc29
K R I S H N A <krishnakripa34567@gmail.com>
2023-09-09 16:22:10 +0530

    Update kernelsu.sh

--- kernelsu.sh
+++ kernelsu.sh
@@ -11,23 +11,23 @@ ZYCLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/17.0.0-20230
 ZYCLANG_DIR="$WORKDIR/ZyClang/bin"
 
 # Kernel Source
-KERNEL_GIT="https://github.com/helliscloser/kernel_xiaomi_sm6150.git"
-KERNEL_BRANCHE="13"
-KERNEL_DIR="$WORKDIR/KenvyraKernel"
+KERNEL_GIT="https://github.com/pure-soul-kk/zelo_kernel_xiaomi_sm6150"
+KERNEL_BRANCHE="zelo-13"
+KERNEL_DIR="$WORKDIR/ZeloKernel"
 
 # Anykernel3
-ANYKERNEL3_GIT="https://github.com/AMWolfstein/AnyKernel3.git"
+ANYKERNEL3_GIT="https://github.com/pure-soul-kk/AnyKernel3.git"
 ANYKERNEL3_BRANCHE="master"
 
 # Build
-DEVICES_CODE="davinci"
-DEVICE_DEFCONFIG="davinci_defconfig"
+DEVICES_CODE="sweet"
+DEVICE_DEFCONFIG="sweet_defconfig"
 DEVICE_DEFCONFIG_FILE="$KERNEL_DIR/arch/arm64/configs/$DEVICE_DEFCONFIG"
 IMAGE="$KERNEL_DIR/out/arch/arm64/boot/Image.gz"
 DTB="$KERNEL_DIR/out/arch/arm64/boot/dtb.img"
 DTBO="$KERNEL_DIR/out/arch/arm64/boot/dtbo.img"
 
-export KBUILD_BUILD_USER=AMWolfstein
+export KBUILD_BUILD_USER=krishna
 export KBUILD_BUILD_HOST=GitHubCI
 
 msg() {
@@ -116,8 +116,8 @@ cp $DTB $WORKDIR/Anykernel3/dtb
 cp $DTBO .
 
 # PACK FILE
-time=$(TZ='Africa/Cairo' date +"%Y-%m-%d %H:%M:%S")
-cairo_time=$(TZ='Africa/Cairo' date +%Y%m%d%H)
+time=$(TZ='Asia/Kolkata' date +"%Y-%m-%d %H:%M:%S")
+cairo_time=$(TZ='Asia/Kolkata' date +%Y%m%d%H)
 ZIP_NAME="KenvyraKernel-$KERNEL_VERSION-KernelSU-$KERNELSU_VERSION.zip"
 find ./ * -exec touch -m -d "$time" {} \;
 zip -r9 $ZIP_NAME *
@@ -125,8 +125,8 @@ mkdir -p $WORKDIR/out && cp *.zip $WORKDIR/out
 
 cd $WORKDIR/out
 echo "
-### Kenvyra KERNEL With/Without KERNELSU
-1. **Time** : $(TZ='Africa/Cairo' date +"%Y-%m-%d %H:%M:%S") # Cario TIME
+### Zelo KERNEL With/Without KERNELSU
+1. **Time** : $(TZ='Asia/Kolkata' date +"%Y-%m-%d %H:%M:%S") # Asia TIME
 2. **Device Code** : $DEVICES_CODE
 3. **LINUX Version** : $KERNEL_VERSION
 4. **KERNELSU Version**: $KERNELSU_VERSION
@@ -134,15 +134,15 @@ echo "
 6. **LLD Version**: $LLD_VERSION
 " > RELEASE.md
 echo "
-### Kenvyra KERNEL With/Without KERNELSU
-1. **Time** : $(TZ='Africa/Cairo' date +"%Y-%m-%d %H:%M:%S") # Cario TIME
+### Zelo KERNEL With/Without KERNELSU
+1. **Time** : $(TZ='Asia/Kolkata' date +"%Y-%m-%d %H:%M:%S") # Asia TIME
 2. **Device Code** : $DEVICES_CODE
 3. **LINUX Version** : $KERNEL_VERSION
 4. **KERNELSU Version**: $KERNELSU_VERSION
 5. **CLANG Version**: ZyC clang version 17.0.0
 6. **LLD Version**: LLD 17.0.0
 " > telegram_message.txt
-echo "KenvyraKernel-$KERNEL_VERSION" > RELEASETITLE.txt
+echo "Zelo@krishna-$KERNEL_VERSION" > RELEASETITLE.txt
 cat RELEASE.md
 cat telegram_message.txt
 cat RELEASETITLE.txt
